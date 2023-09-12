2714dc8dbaf606c36f0c99b2650b098ae4595c49
Marilyne Naili <marilyne.lydia@gmail.com>
2023-09-12 10:03:52 +0200

    git init

---
+++ src/app/pokemon.interface.ts
@@ -0,0 +1,10 @@
+export interface PokemonInterface {
+
+    id: number;
+    hp: number;
+    cp: number;
+    name: string;
+    picture: string;
+    types: string;
+    created: string;
+}
---
+++ src/app/pokemon.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { PokemonInterface } from './pokemon.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor(private http: HttpClient) { }
+
+  getPokemon(): Observable<PokemonInterface> {
+    return this.http.get<PokemonInterface>('http://vps204.tyrolium.fr/apiPokemon/index.php?controller=pokemon&task=getAll')
+  }
+}
